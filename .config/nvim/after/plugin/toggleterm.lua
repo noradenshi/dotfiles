5458f72bbce7f1f24c21ad2856c9f7c807800eaa
noradenshi <74954748+noradenshi@users.noreply.github.com>
2024-05-25 00:44:18 +0200

    toggleterm config

---
+++ .config/nvim/after/plugin/toggleterm.lua
@@ -0,0 +1,9 @@
+require("toggleterm").setup {
+    open_mapping = [[<c-\>]],
+    shader_terminals = true,
+    autochdir = true,
+    direction = 'float',
+    float_opts = {
+        border = 'curved',
+    }
+}
