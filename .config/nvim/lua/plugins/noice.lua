6cf44f2ea1059cc6a6e3c750d7f7a482f807b106
noradenshi <74954748+noradenshi@users.noreply.github.com>
2024-06-26 16:35:55 +0200

    notifybackground colour set

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -2,7 +2,6 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
-        -- add any options here
         presets = {
             command_palette = false, -- position the cmdline and popupmenu together
             lsp_doc_border = true,   -- add a border to hover docs and signature help
@@ -16,9 +15,14 @@ return {
     dependencies = {
         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        -- "rcarriga/nvim-notify",
+
+        {
+            "rcarriga/nvim-notify",
+            config = function()
+                require "notify".setup {
+                    background_colour = "#181818"
+                }
+            end
+        }
     }
 }
